2668b5cc774a0e38b5d8b5147cd1a0bc8acd9e4a
ChisomSylvia <chisomsylvia95@gmail.com>
2024-09-17 17:09:00 +0100

    Added transaction

--- src/routes/webhook.route.js
+++ src/routes/webhook.route.js
@@ -1,13 +1,8 @@
 import { Router } from "express";
 const router = Router();
-import { initializePaystackPaymentCtrl } from "../controllers/transaction.controller.js";
-import { authenticate } from "../middlewares/authenticate.middleware.js";
+import { verifyPayment } from "../controllers/transaction.controller.js";
 // import { USER_TYPES } from "../utils/user.js";
 
-router.post(
-  "/paystack",
-  authenticate([]),
-  initializePaystackPaymentCtrl
-);
+router.post("/paystack", verifyPayment);
 
 export default router;
